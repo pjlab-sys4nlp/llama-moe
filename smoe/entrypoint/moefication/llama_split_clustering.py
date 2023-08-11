e48e8a8c9eab90ca6c3ffc8ab672fb7e6a42cc9e
zhutong <tzhu1997@outlook.com>
2023-08-11 16:14:24 +0800

    remove redundant files

--- smoe/entrypoint/moefication/llama_split_clustering.py
+++ smoe/entrypoint/moefication/llama_split_clustering.py
@@ -15,23 +15,23 @@ if __name__ == "__main__":
     parser.add_argument('--num_experts', type=int, default=8, help='number of experts')
     parser.add_argument('--metric', type=str, default="l2", choices=("l2", "cos"))
 
-args = parser.parse_args()
-args.save_path = os.path.join(
-    args.save_path,
-    os.path.split(args.model_path)[1]
-    + "-"
-    + str(args.num_experts)
-    + "Expert-Split-Clustering",
-)
+    args = parser.parse_args()
+    args.save_path = os.path.join(
+        args.save_path,
+        os.path.split(args.model_path)[1]
+        + "-"
+        + str(args.num_experts)
+        + "Expert-Split-Clustering",
+    )
 
-print("Loading llama model...")
-model = LlamaForCausalLM.from_pretrained(args.model_path).model
+    print("Loading llama model...")
+    model = LlamaForCausalLM.from_pretrained(args.model_path).model
 
-templates = args.templates.split(",")
-for template in templates:
-    for i in tqdm.tqdm(range(model.config.num_hidden_layers)):
-        split = ClusteringSplit(args, model, template, i)
-        split.split()
-        split.cnt()
-        split.save()
-print("Done.")
+    templates = args.templates.split(",")
+    for template in templates:
+        for i in tqdm.tqdm(range(model.config.num_hidden_layers)):
+            split = ClusteringSplit(args, model, template, i)
+            split.split()
+            split.cnt()
+            split.save()
+    print("Done.")
