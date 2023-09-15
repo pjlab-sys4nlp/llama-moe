fix grad=None bug when gradient checkpointing, fix tb logging, update `remote_breakpoint`

smoe/callbacks/tensorboard.py
@@ -31,10 +31,18 @@ class EnhancedTensorboardCallback(TensorBoardCallback):
                         token_loss_key = "train/loss_on_tokens"
                         self.tb_writer.add_scalar(token_loss_key, v, tokens)
                 elif (
+                    k == "train/balance_loss"
+                    and isinstance(v, torch.Tensor)
+                    and hasattr(v, "item")
+                ):
+                    self.tb_writer.add_scalar(k, v.item(), state.global_step)
+                elif (
                     k == "train/num_dropped_tokens"
                     and isinstance(v, tuple)
-                    and all(isinstance(n, (int, float)) for n in v)
+                    and all(isinstance(n, torch.Tensor) for n in v)
                 ):
+                    # (tensor(1.0), tensor(2.3)) -> [1.0, 2.3]
+                    v = [n.item() for n in v]
                     self.tb_writer.add_scalars(
                         f"{k}/layer",
                         {str(i): n for i, n in enumerate(v)},
@@ -42,19 +50,18 @@ class EnhancedTensorboardCallback(TensorBoardCallback):
                     )
                     self.tb_writer.add_scalar(f"{k}/total", sum(v), state.global_step)
                 elif (
-                    k == "train/gate_load"
-                    or k == "train/gate_importance"
+                    (k == "train/gate_load" or k == "train/gate_importance")
                     and isinstance(v, tuple)
-                    and all(isinstance(n, list) for n in v)
+                    and all(isinstance(n, torch.Tensor) for n in v)
                 ):
-                    _v = [torch.tensor(n) for n in v]
+                    # v: (tensor([1.0, 2.3, ... num_experts]), tensor([3.0, 4.5, ... num_experts]), ... num_layers)
                     self.tb_writer.add_scalars(
                         f"{k}/std/layer",
-                        {str(i): n.std().item() for i, n in enumerate(_v)},
+                        {str(i): n.std().item() for i, n in enumerate(v)},
                         state.global_step,
                     )
                     self.tb_writer.add_image(
-                        k, get_heatmap_img_grid_for_tb(_v), state.global_step
+                        k, get_heatmap_img_grid_for_tb(v), state.global_step
                     )
                 else:
                     logger.warning(

smoe/modules/moe/moe_calculators.py
@@ -66,7 +66,7 @@ class UniversalCalculator(nn.Module):
         zeros = torch.zeros((batch_size, output_dim), device=cat_expert_outputs.device, dtype=cat_expert_outputs.dtype)
         y = zeros.index_add(0, sorted_batch_indices, cat_expert_outputs)  # 按照对应的batch编号，添加输出
 
-        return CalculatorOutput(hidden_states=y)
+        return CalculatorOutput(hidden_states=y, num_dropped_tokens=torch.tensor(-1.0))
         # fmt: on
 
 

smoe/modules/moe/moe_gates.py
@@ -142,8 +142,8 @@ class TopKBalancedNoisyGate(nn.Module):
             "topK_indices": top_k_indices,
             "topK_scores": top_k_scores,
             "balance_loss": balance_loss,
-            "load": load.tolist(),
-            "importance": importance.tolist(),
+            "load": load,
+            "importance": importance,
         }
 
     def forward_return_scores(self, x):
@@ -224,7 +224,7 @@ class SwitchBalancedGate(nn.Module):
         gate_network="mlp",
         use_softmax=True,
         use_balance=True,
-        balance_loss_weight=1e-1,
+        balance_loss_weight=1e-2,
         add_noise=True,
     ):
         super(SwitchBalancedGate, self).__init__()
@@ -276,8 +276,8 @@ class SwitchBalancedGate(nn.Module):
             "topK_scores": top1_scores,
             "expert_batch_size": load.tolist(),
             "balance_loss": balance_loss,
-            "load": load_mean.tolist(),
-            "importance": importance_mean.tolist(),
+            "load": load_mean,
+            "importance": importance_mean,
         }
 
     def reset_gate_network(self):

smoe/utils/debugging.py
@@ -1,4 +1,5 @@
 import debugpy
+import torch.distributed as dist
 
 
 def remote_breakpoint(host: str = "0.0.0.0", port: int = 5678):
@@ -40,6 +41,10 @@ def remote_breakpoint(host: str = "0.0.0.0", port: int = 5678):
 
     After the program starts and encounters the breakpoint, you could remote attach the debugger.
     """
-    debugpy.listen((host, port))
-    debugpy.wait_for_client()
-    breakpoint()
+    if dist.is_available() and dist.is_initialized():
+        rank = dist.get_rank()
+        if rank == 0:
+            debugpy.listen((host, port))
+            debugpy.wait_for_client()
+            breakpoint()
+        dist.barrier()